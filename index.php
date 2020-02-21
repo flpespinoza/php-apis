230c870b4dfde432fa3ed6b5190f1426fa83c12a
Felipe Espinoza <flpespinoza@gmail.com>
2020-02-21 19:01:23 +0000

    pregmatch mensajes

--- index.php
+++ index.php
@@ -46,4 +46,58 @@ while($times < 30)
     sleep(1);
 }
 
+
+function obtener_venta_mensaje($e_MSG)
+{
+	$m_RESPUESTA = array();
+	switch($e_MSG)
+	{
+		case (preg_match('#^ *[V]([., ]+T)?[., ]+(10|20|30|50|100|150|200|300|500|1000)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'TELCEL', 'MONTO' => $m_MATCHES[2], 'DESTINO' => $m_MATCHES[3], 'NIP' => $m_MATCHES[4]);
+			break; 
+		case (preg_match('#^ *[V][., ]+TA[., ]+(10|20|30|50)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'TELCEL_ALO', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+TPA[., ]+(30|50|100|150|200|300|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'TELCEL_PA', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+INT[., ]+(5|20|50|100|150|200|300|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'TELCEL_INT', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+M[., ]+(10|20|30|50|60|70|80|100|120|150|200|250|300|400|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'MOVISTAR', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+I[., ]+(10|20|30|50|100|150|200|300|500|1000)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'IUSACELL', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+U[., ]+(10|20|30|50|100|150|200|300|500|1000)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'UNEFON', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+N[., ]+(30|50|100|200|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'NEXTEL', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+V[., ]+(20|30|40|50|100|150|200|300|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'VIRGIN', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+C[., ]+(20|30|50|100|200|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'CIERTO', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+MT[., ]+(10|20|30|60|100|120|150|200|300|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'MAZTIEMPO', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		case (preg_match('#^ *[V][., ]+TU[., ]+(10|20|30|60|100|120|150|200|300|500)[., ]+([0-9]{10})[., ]+([0-9]{4}) *#', $e_MSG, $m_MATCHES) ? true : false):
+			$m_RESPUESTA = array('VENTA' => true, 'OPERADOR' => 'TUENTI', 'MONTO' => $m_MATCHES[1], 'DESTINO' => $m_MATCHES[2], 'NIP' => $m_MATCHES[3]);
+			break;
+		default:
+			$m_RESPUESTA = array('VENTA' => false);
+			break;
+			
+	}
+	
+	return $m_RESPUESTA;
+	
+}
+
+
+
 ?>
