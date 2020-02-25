ace545e88ba1141c86b9b34430ffc5ccdd716c0a
Felipe Espinoza <flpespinoza@gmail.com>
2020-02-25 20:01:30 +0000

    cambio v3

--- telegram_bot_v3.php
+++ telegram_bot_v3.php
@@ -2,18 +2,27 @@
 
 class TelegramBot 
 {
-  private $endPoint,
-          $initTs,
-          $curlObj,
-          $lastUpdate,
-          $currentChat,
-          $currentMsg;
-
-  public $chats;
-  
+  private $endPoint, $curlObj, $lastUpdate;
   public function __construct($token)
   {
     $this->endPoint = "https://api.telegram.org/bot{$token}/";
+    $this->initCurl();
+    //Validar bot
+    try
+    {
+      $this->auth();
+    }
+    catch(Exception $e)
+    {
+      throw new Exception($e->getMessage());
+    }
+
+    //Obtener ultimo mensaje
+    $this->initUpdate();
+  }
+
+  private function initCurl()
+  {
     $this->curlObj = curl_init();
     curl_setopt_array($this->curlObj, array(
       CURLOPT_RETURNTRANSFER => true,
@@ -24,54 +33,25 @@ class TelegramBot
       CURLOPT_CONNECTTIMEOUT => 2,
       CURLOPT_HTTPHEADER     => ["Connection: Keep-Alive", "Keep-Alive: 120"]
     ));
+  }
 
-    try
+  private function auth()
+  {
+    $myInfo = $this->doRequest('getMe');
+    if(!$myInfo['ok'])
     {
-      $this->authBot();
-    }
-    catch(Exception $e)
-    {
-      throw new Exception($e->getMessage());
+      throw new Exception($myInfo['description']);
     }
   }
 
-  private function authBot()
+  private function initUpdate()
   {
-    $me = $this->getMe();
-    if($me['ok'] != true)
+    $updates = $this->getUpdates();
+    if(count($updates))
     {
-      throw new Exception($me['description']);
-    }
-
-    $this->setInitUpdate();
-  }
-
-  private function getMe()
-  {
-    return $this->doRequest('getMe');
-  }
-
-  private function setInitUpdate()
-  {
-    $messages = $this->getUpdates();
-    if(count($messages))
-    {
-      $lastUpdate = $messages[count($messages) - 1]['update_id'];
+      $lastUpdate = $updates[count($updates) - 1]['update_id'];
       $this->setLastUpdate($lastUpdate + 1);
     }
-  }
-
-  private function getUpdates()
-  {
-    $args = ($this->lastUpdate) ? array('offset' => $this->lastUpdate, 'limit' => 1) : array();           
-    $resp = $this->doRequest('getUpdates', $args);
-
-    if($resp['ok'] == true)
-    {
-      return $resp['result'];
-    }
-
-    return array();
   }
 
   private function setLastUpdate($update)
@@ -79,80 +59,17 @@ class TelegramBot
     $this->lastUpdate = $update;
   }
 
-  public function getLastUpdate()
+  private function getUpdates($args = array())
   {
-    return $this->lastUpdate;
-  }
-
-  public function getMessage()
-  {
-    $msg = $this->getUpdates();
-    if(count($msg))
+    $updates = $this->doRequest('getUpdates', $args);
+    if($updates['ok'])
     {
-      return $msg[count($msg) - 1];
+      return $updates['result'];
     }
-    
+
     return array();
   }
 
-  public function chatExists($chat)
-  {
-    return array_key_exists($chat, $this->chats);
-  } 
 
-  public function requestUserData($msgTxt = "Para registrar tu teléfono presiona el boton de Registrar")
-  {
-    $markup = array
-    (
-      'keyboard' => array
-      (
-        array
-        (
-          array
-          (
-            'text' => 'Registrar', 
-            'request_contact' => true
-          )
-        )
-      ),
-      'resize_keyboard' => true,
-      'one_time_keyboard' => true
-    );
 
-    return $this->sendMessage($msgTxt, false, $markup);
-  }
-
-  private function doRequest($methodName, $args = array())
-  {
-    curl_setopt_array($this->curlObj, [
-      CURLOPT_URL        => $this->endPoint . $methodName,
-      CURLOPT_POSTFIELDS => empty($args) ? null : $args,
-    ]);
-
-    $result_curl = curl_exec($this->curlObj);
-    if($result_curl === false)
-    {
-      $resp = array(
-        "ok" => false,
-        "error_code" => curl_errno($this->curlObj),
-        "error_descripcion" => curl_error($this->curlObj),
-        "curl_error" => true
-      );
-      return json_decode(json_encode($resp), true);
-    }
-
-    $resp = json_decode($result_curl, true);
-    if($resp === null )
-    {
-      $arr = [
-        "ok"          => false,
-        "error_code"  => json_last_error(),
-        "description" => json_last_error_msg(),
-        "json_error"  => true
-      ];
-      $resp = json_decode(json_encode($arr), true);
-    }
-
-    return $resp;
-  }
 }
